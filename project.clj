bf3d92fce032fc105c0f4f5b0a705bbc040241c1
João França <joao.francapf@gmail.com>
2020-07-21 20:25:39 -0300

    Added Designer entity and resolvers

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[cider/cider-nrepl "0.24.0"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.walmartlabs/lacinia "0.37.0"]])
   ;:repl-options {:init-ns clojure-game-geek.core})
