3a9e8a9f94a66d31b5b6d1b73c8b0f4fd8f6e22d
João França <joao.francapf@gmail.com>
2020-07-21 21:56:46 -0300

    Added server fuctions and GraphiQL support

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[cider/cider-nrepl "0.24.0"]]
+  ;:plugins [[cider/cider-nrepl "0.24.0"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.walmartlabs/lacinia "0.37.0"]])
+                 [com.walmartlabs/lacinia-pedestal "0.14.0"]
+                 [io.aviso/logging "0.2.0"]])
   ;:repl-options {:init-ns clojure-game-geek.core})
