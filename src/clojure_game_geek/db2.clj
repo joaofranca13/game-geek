c730fe4972ce400e01871c93086779137c6e3df9
João França <joao.francapf@gmail.com>
2020-07-29 23:01:56 -0300

    Deleted uneeded files

--- src/clojure_game_geek/db2.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure-game-geek.db
-  (:require [com.stuartsierra.component :as component]))
-
-
-(def config
-  {:classname "org.postgresql.Driver"
-   :subprotocol "postgresql"
-   :subname "//localhost:5432/clj_game_geek"})
